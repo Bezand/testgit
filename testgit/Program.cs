941a5c42ea756c82b2e0027cc38a8fc4989ace36
Bezand <trus2332@mail.ru>
2020-07-27 11:02:35 +0800

    l

--- testgit/Program.cs
+++ testgit/Program.cs
@@ -4,6 +4,10 @@ namespace testgit
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
